cdfbf0f3ccffda01a9839ea9eb94d02ff3881bd5
@otni.rodrigues <ors@cin.ufpe.br>
2024-06-08 02:09:08 -0300

    Creating entity and repository

---
+++ src/main/java/com/example/loginauthapi/domain/user/User.java
@@ -0,0 +1,28 @@
+package com.example.loginauthapi.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+}
